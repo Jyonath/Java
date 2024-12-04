051b3cbf1d37ac39cb73ea52a41ec20ea83ae0f6
Kusuma Jyonath <145849041+Jyonath@users.noreply.github.com>
2024-12-04 12:40:14 +0530

    profile.java
    java

--- profile.java
+++ profile.java
@@ -8,7 +8,7 @@ String username;
 String password;
 int age;
 float cgpa;
-
+String email;
 void Create()
 {
 Scanner s=new Scanner(System.in);
@@ -27,7 +27,7 @@ age=s.nextInt();
 System.out.println("Enter cgpa:");
 cgpa=s.nextFloat();
 }
-void app()
+void login()
 {
 String uname;
 String psw;
@@ -51,8 +51,61 @@ else
 System.out.println("Invalid username/password plzz retry");
 }
 }
+void yes()
+{
+String uname;
+String psw;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter username:");
+uname=s.next();
+System.out.println("Enter password:");
+psw=s.next();
+if(username.equals(uname) && password.equals(psw))
+{
+System.out.println("Modify your details");
+Create();
+}
+else
+{
+System.out.println("Invalid username/password");
+}
+}
 void edit()
 {
-
+String a;
+String b="yes";
+String c="no";
+Scanner e=new Scanner(System.in);
+System.out.println("Edit profile yes or no?:");
+a=e.next();
+System.out.println("Enter email:");
+email=e.next();
+if(a.equals(b))
+{
+yes();
+}
+else
+{
+System.out.println("ok");
+}
+}
+void update()
+{
+System.out.println("id:"+id);
+System.out.println("firstname:"+fn);
+System.out.println("lastname:"+ln);
+System.out.println("username:"+username);
+System.out.println("password:"+password);
+System.out.println("age:"+age);
+System.out.println("cgpa:"+cgpa);
+System.out.println("Updated successfully!");
+}
+public static void main(String[] ok)
+{
+Profile p=new Profile();
+p.Create();
+p.login();
+p.edit();
+p.update();
 }
 }
