1293aee728168022ca71333c383422cc85955741
Kusuma Jyonath <145849041+Jyonath@users.noreply.github.com>
2024-12-13 10:51:10 +0530

    Operators.java
    java

---
+++ Operators.java
@@ -0,0 +1,26 @@
+package com.mypackage;
+
+public class operator {
+	public static void main(String[] args) {
+		String s1=new String("java");
+		String s2=new String("Java");
+		if(s1==s2)
+		{
+			System.out.println("Strings are equal");
+		}
+		else
+		{
+			System.out.println("Strings are not eqaul");
+		}
+		String s3="python";
+		String s4="python";
+		if(s3==s4)
+		{
+			System.out.println("Strings are equal");
+		}
+		else
+		{
+			System.out.println("Strings are not eqaul");
+		}
+	}
+}
